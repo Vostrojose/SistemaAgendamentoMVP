bb05ba862dc92686a652b49ca30bdf387e988426
Jose M. Silva <josemsilva1984@gmail.com>
2026-05-16 15:26:08 -0300

    MVP completo pronto para deploy

--- backend/src/server.js
+++ backend/src/server.js
@@ -7,11 +7,14 @@
 
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const path = require('path');
 const dbPool = require('./config/db');
 
 // Middlewares para interpretar JSON e dados de formulários HTML
+
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -25,6 +28,7 @@ app.use(
 // Importação das rotas
 const usuariosRoutes = require('./routes/usuarios');
 const agendaRoutes = require('./routes/agenda');
+
 
 // aut
 app.get('/', (req, res) => {
